009ff5945ade5decee463ab01335595580239d22
aBanjo111 <76058655+aBanjo111@users.noreply.github.com>
2021-01-05 20:27:35 -0500

    Add files via upload

---
+++ input.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace key_input_lib
+{
+    public class Input
+    {
+        public static string keypressed = "";
+        public bool Keypressed(string key)
+        {
+            while (true)
+            {
+                ConsoleKeyInfo keypressed = Console.ReadKey();
+                if (key.ToUpperInvariant() == keypressed.Key.ToString())
+                {
+                    return true;
+                }
+                
+            }
+        }
+
+
+    }
+}
